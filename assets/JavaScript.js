fab6be6174b8ade61eda0f11d8e68d1385b7b902
Ryan Melo <68796440+ryan-melo@users.noreply.github.com>
2023-02-03 23:47:02 -0300

    Add files via upload

---
+++ assets/JavaScript.js
@@ -0,0 +1,47 @@
+function teste() {
+	const randonNumber = (Math.round(Math.random() * 10));
+	return randonNumber
+}
+let randonNumber = teste()
+
+let xAttempts = 1;
+
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+console.log(randonNumber)
+function handleClick(event) {
+	event.preventDefault()
+	
+	const inputNumber = document.querySelector("#inputNumber");
+
+      if (Number(inputNumber.value) == randonNumber) {
+			screen1.classList.add("hide")
+			screen2.classList.remove("hide")
+
+			document.querySelector("h2").innerHTML = `Acertou em ${xAttempts}!`
+      } else {
+			document.querySelector(".screen1").classList.remove("hide")
+			document.querySelector(".screen2").classList.add("hide")
+      }
+
+		inputNumber.value = "";
+      xAttempts++;
+
+};
+
+btnTry.addEventListener('click', handleClick);
+
+btnReset.addEventListener("click", () => {
+	screen1.classList.remove("hide")
+	screen2.classList.add("hide")
+
+	xAttempts = 1;
+	randonNumber = teste();
+
+	console.clear()
+	console.log(randonNumber)
+})
